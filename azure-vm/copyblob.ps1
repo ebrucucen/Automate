0fd8f039af7ba1e58b33e8632a992671beb25050
Ebru Cucen <ebrugul@hotmail.com>
2016-08-30 15:41:46 +0100

    $storageAccountName parameter placed

--- azure-vm/copyblob.ps1
+++ azure-vm/copyblob.ps1
@@ -1,6 +1,6 @@
 $rgName = 'name'
 $storageAccountName = 'dicustomimages'
-$customimagebloguri = 'https://<Storageaccountname>.blob.core.windows.net/system/Microsoft.Compute/Images/customvhds/dicustom-osDisk.a50ce7c4-0128-441e-a69f-bf712f4c4d93.vhd'
+$customimagebloguri = "https://$storageAccountName.blob.core.windows.net/system/Microsoft.Compute/Images/customvhds/dicustom-osDisk.a50ce7c4-0128-441e-a69f-bf712f4c4d93.vhd"
 $location = 'North Europe'
 #Create Below only if its first time
 New-AzureRmResourceGroup -Name $rgName -Location $location -Force
@@ -8,7 +8,7 @@ New-AzureRmResourceGroup -Name $rgName -Location $location -Force
 $stoexists = Find-AzureRmResource -ResourceNameContains $storageAccountName 
 if ($stoexists -eq $null)
 { 
-    New-AzureRmStorageAccount -ResourceGroupName $rgName -Name $storageAccountName -Location $location -SkuName Standard_LRS -Verbose
+    New-AzureRmStorageAccount -ResourceGroupName $rgName -Name $storageAccountName -Location $location -SkuName Standard_LRS -Verbose 
     $stoctx = New-AzureStorageContext -StorageAccountName $storageAccountName -StorageAccountKey (Get-AzureRmStorageAccountKey -ResourceGroupName $rgName -Name $storageAccountName).Value[0] 
     New-AzureStorageContainer -Name divhds  -Context $stoctx
 }
